d2cd1248daf077907ee856d5a1123572482ae487
배성진 <qotjdwls66@medicalai.com>
2025-09-29 00:18:25 +0900

    fix: 설정수정

--- lumina/src/lib/googleVision.ts
+++ lumina/src/lib/googleVision.ts
@@ -11,11 +11,6 @@ const client = new ImageAnnotatorClient({
     private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     client_id: process.env.GOOGLE_CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
-    universe_domain: "googleapis.com"
   },
   keyFilename: process.env.GOOGLE_CLOUD_KEY_FILE,
 });
