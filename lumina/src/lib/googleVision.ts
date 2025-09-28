a9fe15a6b0845bc29156ec714cee79a6b252c575
배성진 <qotjdwls66@medicalai.com>
2025-09-29 00:16:38 +0900

    fix: env 설정방식 변경

--- lumina/src/lib/googleVision.ts
+++ lumina/src/lib/googleVision.ts
@@ -4,6 +4,19 @@ import { protos } from '@google-cloud/vision';
 // Google Cloud Vision 클라이언트 초기화
 const client = new ImageAnnotatorClient({
   projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  credentials: {
+    type: "service_account",
+    project_id: process.env.GOOGLE_CLOUD_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    universe_domain: "googleapis.com"
+  },
   keyFilename: process.env.GOOGLE_CLOUD_KEY_FILE,
 });
 
